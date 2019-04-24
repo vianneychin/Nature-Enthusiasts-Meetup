6fb1ce60e20e9c83d2c6a7c2a5fea3d08f7697d0
chaselancaster <chaseblancaster@gmail.com>
2019-04-24 14:21:22 -0700

    Add render in server.js for home.ejs

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const logger = require("morgan");
-require("./db/db")
-
-
+require("./db/db");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.render("home/home.ejs");
+});
 
 app.listen(port, err => {
   console.log(err || "App is listening on port", port);
