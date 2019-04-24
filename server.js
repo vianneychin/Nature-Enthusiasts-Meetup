66cab76007476246eff426b99640f050fb159728
chaselancaster <chaseblancaster@gmail.com>
2019-04-24 10:19:55 -0700

    Set up basic server is listening on port 3000

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-const x = 5;
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, err => {
+  console.log(err || "App is listening on port", port);
+});
