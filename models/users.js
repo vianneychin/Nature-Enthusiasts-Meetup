773e6ba9df75c5f6820c59bf5430be7ac63c3df8
chaselancaster <chaseblancaster@gmail.com>
2019-04-24 11:35:49 -0700

    Add monogoose schema and ref in user events model

--- models/users.js
+++ models/users.js
@@ -4,7 +4,12 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
-  events: []
+  events: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Events"
+    }
+  ]
 });
 
 const User = mongoose.model("User", userSchema);
