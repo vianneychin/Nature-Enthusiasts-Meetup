47d5a41227ce309de7c846eede91207044234bf1
chaselancaster <chaseblancaster@gmail.com>
2019-04-24 14:47:21 -0700

    Add signup route in user routes and signup function in user controller

--- controllers/users.js
+++ controllers/users.js
@@ -3,5 +3,8 @@ const User = require("../models/users");
 module.exports = {
   login: (req, res) => {
     res.render("users/login.ejs");
+  },
+  signup: (req, res) => {
+    res.render("users/signup.ejs");
   }
 };
