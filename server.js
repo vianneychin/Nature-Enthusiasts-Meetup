75f5622a29d0d249e6aa841de3f71bb382b52e44
chaselancaster <chaseblancaster@gmail.com>
2019-04-24 10:26:21 -0700

    Require bodyParser, methodOverride, logger, and set up middleware

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const logger = require("morgan");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(logger("dev"));
 
 app.listen(port, err => {
   console.log(err || "App is listening on port", port);
