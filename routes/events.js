c9dbb3093baee306696afb0e2787ec6ac0f57e3b
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-24 20:14:39 -0700

    add event routes

--- routes/events.js
+++ routes/events.js
@@ -4,5 +4,6 @@ const eventController = require("../controllers/events");
 
 router.get("/", eventController.index);
 router.get("/new", eventController.new);
+router.get("/show/:id", eventController.show)
 
 module.exports = router;
