79f4ca56b2d6257adb7ea9f9441ff4f7372b8564
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-04-24 21:26:17 -0700

    adding more events routes and views ejs's

--- controllers/events.js
+++ controllers/events.js
@@ -2,16 +2,23 @@ const Event = require("../models/events");
 
 
 module.exports = {
-  index: (req, res) => {
-    res.render("events/index.ejs", 
-    {
-      event: Event
-    });
-  },
-  new: (req, res) => {
+  index: async(req, res) => {
+    try {
+      const foundEvent = await Event.find({})
+      res.render("events/index.ejs", 
+      {
+        event: Event
+      }) 
+      }
+      catch (err){
+      res.render(err)
+      }
+    },
+    new: (req, res) => {
     res.render("events/new.ejs");
-  },
-  show: (req, res) => {
-    res.render("events/show.ejs")
-  }
-};
+    }
+    }
+//   show: (req, res) => {
+//     res.render("events/show.ejs")
+//   }
+// };
