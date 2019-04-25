54e46c90e190511c6354ff31b6a75c8b517ea7ae
chaselancaster <chaseblancaster@gmail.com>
2019-04-25 10:47:30 -0700

    Require session in server.js and app.use is. Create initial file for authController and auth route

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ const methodOverride = require("method-override");
 const logger = require("morgan");
 const userRouter = require("./routes/users");
 const eventRouter = require("./routes/events");
+const session = require("express-session");
 require("./db/db");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // Express can take in json data
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "Random string",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.use(logger("dev"));
 // adding static style
 app.use(express.static("public"));
