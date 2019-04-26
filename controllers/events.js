34f2ca5b4391d1b16735f68da6b798b3f1ba9182
chaselancaster <chaseblancaster@gmail.com>
2019-04-25 19:54:43 -0700

    Add event show function and update index href.  It functions now

--- controllers/events.js
+++ controllers/events.js
@@ -1,47 +1,40 @@
 const Event = require("../models/events");
 
-
 module.exports = {
-  index: async(req, res) => {
+  index: async (req, res) => {
     try {
-      const allEvents = await Event.find({})
-      res.render("events/index.ejs",{
-          event: allEvents
-        }) 
-      } 
-      catch(err){
-      res.send(err)
-      }
-    },
+      const allEvents = await Event.find({});
+      res.render("events/index.ejs", {
+        event: allEvents
+      });
+    } catch (err) {
+      res.send(err);
+    }
+  },
 
   new: (req, res) => {
     res.render("events/new.ejs");
-    },
-  create: async( req, res) => {
+  },
+  create: async (req, res) => {
     try {
-      console.log(req.body)
-      const newEvent = await Event.create(req.body)
-        console.log(newEvent)
-        res.redirect("/events")
-      }
-    catch(err) {
-      res.render(err)
+      console.log(req.body);
+      const newEvent = await Event.create(req.body);
+      console.log(newEvent);
+      res.redirect("/events");
+    } catch (err) {
+      res.render(err);
     }
-    },
-    show: (req,res)=> {
-      console.log("this function works")
+  },
+  show: async (req, res) => {
+    try {
+      const foundEvent = await Event.findById(req.params.id);
+      console.log(foundEvent);
+      res.render("events/show.ejs", {
+        event: foundEvent
+        // sessionId: req.session.usersDbId
+      });
+    } catch (err) {
+      res.send(err);
     }
-    // show: async(req,res) => {
-    //   try { 
-    //     const foundEvent = await Event.findById(req.params.id)
-    //     console.log(foundEvent, "<====== the found event")
-    //     res.render("events/show.ejs", {
-    //       event: foundEvent
-    //     })
-    //   }
-    //   catch(err) {
-    //     res.render(err)
-    //   }
-    // }
-}
-  
+  }
+};
