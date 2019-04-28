2b51fb3cf6770d445f5f4e09d2a64aef4f83592a
chaselancaster <chaseblancaster@gmail.com>
2019-04-28 13:38:49 -0700

    Add variable in app.js to represent the yesBtn before making cancelBtn

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 // #TeamVanillaJS
 
 // Event listener for the "Yes" button on the even show page
-document.querySelector("yes-btn").addEventListener(console.log("btn clicked"));
+const yesBtn = document.querySelector(".yes-btn");
+
+yesBtn.addEventListener(console.log("btn clicked"));
